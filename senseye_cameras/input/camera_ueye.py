dfcc46f8ae3493208d3dc15d6e632ca82b9dbb30
Allen Zhang <allen.zhang@senseye.co>
2020-02-05 16:58:39 -0600

    working

--- senseye_cameras/input/camera_ueye.py
+++ senseye_cameras/input/camera_ueye.py
@@ -5,7 +5,9 @@ import numpy as np
 from . input import Input
 from pyueye import ueye
 
+logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
+
 
 class CameraUeye(Input):
     '''
@@ -23,54 +25,61 @@ class CameraUeye(Input):
 
         self.input = ueye.HIDS(0)
 
-    def open(self):
-        '''Opens raw video as a bytes file.'''
-        # initialize camera
-        if(ueye.is_InitCamera(self.input, None) != ueye.IS_SUCCESS):
-            log.error("is_InitCamera ERROR")
-
-
-
-        pcImageMemory = ueye.c_mem_p()
-        MemID = ueye.int()
-        pitch = ueye.INT()
-        channels = 3                    #3: channels for color mode(RGB); take 1 channel for monochrome
-
-        # get the color mode
-        sensor_info = ueye.SENSORINFO()
-        color_mode = int.from_bytes(sensor_info.nColorMode.value, byteorder='big')
-        m_nColorMode = ueye.INT()		# Y8/RGB16/RGB24/REG32
-
-        # determine the number of bits/bytes per pixel through the color mode
-        nBitsPerPixel = ueye.INT(24)
-        if color_mode == ueye.IS_COLORMODE_BAYER:
-            # setup the color depth to the current windows setting
-            ueye.is_GetColorDepth(self.input, nBitsPerPixel, m_nColorMode)
-        elif color_mode == ueye.IS_COLORMODE_CBYCRY:
-            m_nColorMode = ueye.IS_CM_BGRA8_PACKED
-            nBitsPerPixel = ueye.INT(32)
-        elif color_mode == ueye.IS_COLORMODE_MONOCHROME:
-            m_nColorMode = ueye.IS_CM_MONO8
-            nBitsPerPixel = ueye.INT(8)
-        else:
-            m_nColorMode = ueye.IS_CM_MONO8
-            nBitsPerPixel = ueye.INT(8)
-        bytes_per_pixel = int(nBitsPerPixel / 8)
-
-
-        # determine width/height of the image
-        # can also be used to set an area of interest
-        # Can be used to set the size and position of an "area of interest"(AOI) within an image
+    def initialize_dimensions(self):
+        '''
+        Gets dimensions of the camera.
+        Sets:
+            self.width
+            self.height
+        '''
+        log.info('Getting camera dimensions...')
         rectAOI = ueye.IS_RECT()
         nRet = ueye.is_AOI(self.input, ueye.IS_AOI_IMAGE_GET_AOI, rectAOI, ueye.sizeof(rectAOI))
         if nRet != ueye.IS_SUCCESS:
             print("is_AOI ERROR")
-        width = rectAOI.s32Width
-        height = rectAOI.s32Height
+        self.width = rectAOI.s32Width
+        self.height = rectAOI.s32Height
 
+    def initialize_color_mode(self):
+        '''
+        Initializes color mode.
+        Sets:
+            self.m_nColorMode
+            self.bits_per_pixel
+            self.bytes_per_pixel
+        '''
+        sensor_info = ueye.SENSORINFO()
+        color_mode = int.from_bytes(sensor_info.nColorMode.value, byteorder='big')
+        self.m_nColorMode = ueye.INT()		# Y8/RGB16/RGB24/REG32
 
+        # determine the number of bits/bytes per pixel through the color mode
+        bits_per_pixel = ueye.INT(24)
+        if color_mode == ueye.IS_COLORMODE_BAYER:
+            # setup the color depth to the current windows setting
+            ueye.is_GetColorDepth(self.input, bits_per_pixel, self.m_nColorMode)
+        elif color_mode == ueye.IS_COLORMODE_CBYCRY:
+            self.m_nColorMode = ueye.IS_CM_BGRA8_PACKED
+            bits_per_pixel = ueye.INT(32)
+        elif color_mode == ueye.IS_COLORMODE_MONOCHROME:
+            self.m_nColorMode = ueye.IS_CM_MONO8
+            bits_per_pixel = ueye.INT(8)
+        else:
+            self.m_nColorMode = ueye.IS_CM_MONO8
+            bits_per_pixel = ueye.INT(8)
+        self.bytes_per_pixel = int(bits_per_pixel / 8)
+        self.bits_per_pixel = bits_per_pixel
+
+    def initialize_memory(self):
+        '''
+        Allocates image memory.
+        Sets:
+            self.MemID
+            self.pcImageMemory
+        '''
         # Allocates an image memory for an image having its dimensions defined by width and height and its color depth defined by nBitsPerPixel
-        nRet = ueye.is_AllocImageMem(self.input, width, height, nBitsPerPixel, pcImageMemory, MemID)
+        MemID = ueye.int()
+        pcImageMemory = ueye.c_mem_p()
+        nRet = ueye.is_AllocImageMem(self.input, self.width, self.height, self.bits_per_pixel, pcImageMemory, MemID)
         if nRet != ueye.IS_SUCCESS:
             print("is_AllocImageMem ERROR")
         else:
@@ -80,34 +89,43 @@ class CameraUeye(Input):
                 print("is_SetImageMem ERROR")
             else:
                 # Set the desired color mode
-                nRet = ueye.is_SetColorMode(self.input, m_nColorMode)
+                nRet = ueye.is_SetColorMode(self.input, self.m_nColorMode)
+        self.MemID = MemID
+        self.pcImageMemory = pcImageMemory
 
+    def initialize_modes(self):
+        '''
+        Enables live video mode.
+        Enables queue mode.
+        Sets:
+            self.pitch
+        '''
         # Activates the camera's live video mode (free run mode)
         nRet = ueye.is_CaptureVideo(self.input, ueye.IS_DONT_WAIT)
         if nRet != ueye.IS_SUCCESS:
             print("is_CaptureVideo ERROR")
 
         # Enables the queue mode for existing image memory sequences
-        nRet = ueye.is_InquireImageMem(self.input, pcImageMemory, MemID, width, height, nBitsPerPixel, pitch)
+        self.pitch = ueye.INT()
+        nRet = ueye.is_InquireImageMem(self.input, self.pcImageMemory, self.MemID, self.width, self.height, self.bits_per_pixel, self.pitch)
         if nRet != ueye.IS_SUCCESS:
             print("is_InquireImageMem ERROR")
 
-        self.pcImageMemory = pcImageMemory
-        self.width = width
-        self.height = height
-        self.nBitsPerPixel = nBitsPerPixel
-        self.pitch = pitch
-        self.bytes_per_pixel = bytes_per_pixel
-        self.pcImageMemory = pcImageMemory
-        self.MemID = MemID
+    def open(self):
+        '''Opens and initializes ueye camera.'''
+        # initialize camera
+        if(ueye.is_InitCamera(self.input, None) != ueye.IS_SUCCESS):
+            log.error("is_InitCamera ERROR")
 
+        self.initialize_color_mode()
+        self.initialize_dimensions()
+        self.initialize_memory()
+        self.initialize_modes()
+
+        # ueye.is_Exposure(self.input, ueye.IS_EXPOSURE_CMD_SET_EXPOSURE, ueye.double(25.0), 8)
 
     def read(self):
-        '''
-        Reads in raw video.
-        config['res'] dictates how many bytes are read in.
-        '''
-        array = ueye.get_data(self.pcImageMemory, self.width, self.height, self.nBitsPerPixel, self.pitch, copy=False)
+        array = ueye.get_data(self.pcImageMemory, self.width, self.height, self.bits_per_pixel, self.pitch, copy=False)
         frame = np.reshape(array, (self.height.value, self.width.value, self.bytes_per_pixel))
         return frame, time.time()
 
