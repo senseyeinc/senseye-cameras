b8508f169e4b78a0825ab34b20be6d3ad3cde3cc
Allen Zhang <allen.zhang@senseye.co>
2020-02-06 15:02:49 -0600

    modded config

--- examples/stream/pyueye_to_raw.py
+++ examples/stream/pyueye_to_raw.py
@@ -2,7 +2,12 @@ import time
 from senseye_cameras import Stream
 
 s = Stream(
-    input_type='ueye', id=0,
+    input_type='ueye', id=0, input_config={
+        'fps': 60,
+        'exposure': 60,
+        'autofocus': 1,
+        'autogain': 1,
+    },
     output_type='raw', path='./tmp/ueye.raw',
     reading=True,
     writing=True,
