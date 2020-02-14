f63bd32dc3424d9bc2f4f5d0998aec76b55e56f3
Allen Zhang <allen.zhang@senseye.co>
2020-02-14 14:46:13 -0600

    avi

--- examples/stream/usb_to_file.py
+++ examples/stream/usb_to_file.py
@@ -7,11 +7,11 @@ from senseye_cameras import Stream
 CAMERA_ID = 0
 
 # supported file extensions
-# FILE_PATH = './tmp/usb.avi'
+FILE_PATH = './tmp/usb.avi'
 # FILE_PATH = './tmp/usb.mp4'
 # FILE_PATH = './tmp/usb.mkv'
 # FILE_PATH = './tmp/usb.yuv'
-FILE_PATH = './tmp/usb.raw'
+# FILE_PATH = './tmp/usb.raw'
 
 s = Stream(
     input_type='usb', id=CAMERA_ID,
